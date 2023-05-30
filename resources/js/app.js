9821baaec618ffb2d3ee47aad309f5d13850a350
Douglas Smith <smithda@gmail.com>
2023-05-30 07:40:27 -0400

    Installing Inertia.js (client-side)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 import './bootstrap';
+// import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+const appName =
+  window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel Inertia Template';
+
+createInertiaApp({
+  title: (title) => `${title} | ${appName}`,
+  resolve: (name) =>
+    resolvePageComponent(
+      `./Pages/${name}.vue`,
+      import.meta.glob('./Pages/**/*.vue', { eager: true }),
+    ),
+  setup({ el, App, props, plugin }) {
+    return createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el);
+  },
+  progress: {
+    color: '#000000',
+  },
+});
