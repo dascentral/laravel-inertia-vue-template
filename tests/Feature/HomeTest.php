1af7dc1921b51756efa87f175d949046f32d65e3
Douglas Smith <smithda@gmail.com>
2023-09-04 13:41:29 -0400

    Turning ExampleTest into HomeTest

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -1,6 +1,6 @@
 <?php
 
-it('returns a successful response', function () {
+it('returns a successful response for the home page', function () {
     $response = $this->get('/');
 
     $response->assertStatus(200);
