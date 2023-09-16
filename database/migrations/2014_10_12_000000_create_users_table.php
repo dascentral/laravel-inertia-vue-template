632af529790b08d9413e0fb347bc73910f3565db
Douglas Smith <smithda@gmail.com>
2023-09-16 07:14:55 -0400

    Using custom column names for timestamp fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -19,7 +20,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+
+            $table->timestamp('date_created')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('last_updated')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
