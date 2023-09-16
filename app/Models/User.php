09085a5e8de030a0ffa23468bc14397865e71ec7
Douglas Smith <smithda@gmail.com>
2023-09-16 07:15:15 -0400

    Replacing fillable with guarded

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,18 +13,14 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
     public const CREATED_AT = 'date_created';
 
     public const UPDATED_AT = 'last_updated';
+
+    protected $guarded = [
+        'id',
+        'date_created',
+        'last_updated',
     ];
 
     protected $hidden = [
